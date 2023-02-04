ea6f70e9792e4e1eef99b4230e8e5539ef55c813
am7u19 <am7u19@soton.ac.uk>
2023-02-04 23:59:03 +0000

    Init commit of angular front-end

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTabsModule } from "@angular/material/tabs";
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatTabsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
